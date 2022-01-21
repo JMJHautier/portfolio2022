570561eb653c9bda5db64d988eb1d8ba99f2c698
Jean <jhautier@hotmail.fr>
2022-01-21 18:14:43 +0100

    fix(picklangauge): fix call to picklanguage

--- src/components/PickLanguage/index.js
+++ src/components/PickLanguage/index.js
@@ -1,15 +1,16 @@
 import { pickLanguage as handleClick } from "../../functions/pickLanguage"
+import './PickLanguage.scss'
 
-const PickLanguage = ()=>{
+const PickLanguage = ({setLanguage})=>{
 
 return <div className="PickLanguage__container"> 
-  <button onClick = {handleClick} id="EN">
+  <button onClick = {(event)=> handleClick(event, setLanguage)} id="EN">
   Stick to english please!
   </button>
-  <button onClick = {handleClick}  id="DE">
+  <button onClick = {(event)=> handleClick(event, setLanguage)}  id="DE">
   lieber auf Deutsch, danke!
   </button>
-  <button onClick = {handleClick} id="FR">
+  <button onClick = {(event)=> handleClick(event, setLanguage)} id="FR">
   en français, bien sur! 
   </button>
 </div>
