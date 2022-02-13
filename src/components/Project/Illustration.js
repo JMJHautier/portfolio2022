eae63c2edbcdb02c7456cf091409a5a51b69a5be
Jean <jhautier@hotmail.fr>
2022-02-13 11:29:41 +0100

    feat(projects): add toggle

--- src/components/Project/Illustration.js
+++ src/components/Project/Illustration.js
@@ -1,9 +1,9 @@
 import { CSSTransition } from "react-transition-group"
+import TransparentButton from "../Buttons/Transparent"
 
-const Illustration = ({pic, title, transitionIsReset})=>{
+const Illustration = ({pic, title, transitionIsReset, switchToggle})=>{
 
 return <div className="project__illustration">
-  <h2>{title}</h2>
   <CSSTransition in={transitionIsReset} timeout={800} classNames={{enter:"illustration-enter", enterActive:"illustration-enter-active", exit:"illustration-exit", exitActive:"illustration-exit-active"}}>
 
   <div className="illustration_wrapper">
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react"
 import Illustration from "./Illustration"
 import TabSystem from "./TabSystem"
+import useToggle from '../../hooks/useToggle'
 import './Project.scss'
 import { CSSTransition } from "react-transition-group"
+import TransparentButton from "../Buttons/Transparent"
 
 const Project = ({tabs, projectTitle})=>{
   const [transitionIsReset, setTransitionIsReset] = useState(true)
-
+  const homePic = "./pictures/memoria/Home-project.png"
+  const [toggled, switchToggle] = useToggle(false)
   const [tab1, tab2, tab3, tab4] = tabs
   const [activeTab, setActiveTab] = useState(tab1)
   const [inactiveTabs, setInactiveTabs] = useState([tab2, tab3, tab4])
@@ -15,16 +18,28 @@ const Project = ({tabs, projectTitle})=>{
     setTimeout(()=> {
       setTransitionIsReset(true)
     }, 200) 
-  }, [activeTab.pic])
+  }, [activeTab.pic, toggled])
 
 return <div className="project"> 
     <div className="project__container"> 
+
       <div className="project__left-side">
-          <Illustration pic={activeTab.pic} title={projectTitle} transitionIsReset={transitionIsReset}/>
+          {toggled&&<h2><TransparentButton onClick={switchToggle}>retour</TransparentButton></h2>}
+
+          <Illustration switchToggle={switchToggle} pic={toggled?activeTab.pic:homePic} title={projectTitle} transitionIsReset={transitionIsReset}/>
       </div>
       <div className="project__right-side">
-
+      <CSSTransition in={toggled} timeout={800} classNames={{enter:"illustration-enter", enterActive:"illustration-enter-active", exit:"illustration-exit", exitActive:"illustration-exit-active"}} unmountOnExit>
         <TabSystem transitionIsReset={transitionIsReset} activeTab={activeTab} setActiveTab={setActiveTab} tabs={inactiveTabs} setTabs={setInactiveTabs} /> 
+      </CSSTransition>
+      <CSSTransition in={!toggled} timeout={800} classNames={{enter:"illustration-enter", enterActive:"illustration-enter-active", exit:"illustration-exit", exitActive:"illustration-exit-active"}} unmountOnExit>
+
+        <div> 
+          my content 
+          <TransparentButton onClick={switchToggle}>En savoir plus</TransparentButton>
+          </div> 
+          </CSSTransition>
+
       </div>
     </div>
   </div>
