7403cf8576466a6d4ac87615e5e512b0eab3f45c
Jean <jhautier@hotmail.fr>
2022-03-30 18:14:46 +0200

    add hyperlinks to websites

--- src/text/projects/presentation/index.js
+++ src/text/projects/presentation/index.js
@@ -5,20 +5,20 @@ import SetTranslate from "../../SetTranslate";
 
 export const memoria = (<SetTranslate>
 {{
-FR:`*memOria* est une **application de carte de révision, pensée pour les développeurs.**  
+FR:`*[memOria](https://memoria-oclock.netlify.app/)* est une **application de carte de révision, pensée pour les développeurs.**  
 \\
 **J’y suis en charge du front**, et travaille en collaboration avec [@S-Coulibally](https://github.com/S-COULIBALY) et [@VincentMingam](https://github.com/vmingam) pour le back.
 \\
 **Techno** :  CSS  - SASS  / JS - React / node.js – express / SQL – postgres [![Github](./logo/github.png)](https://github.com/memOria-project/memOria)
 
 `,
-EN:`*memOria* is a **flashcard application** designed for developers.
+EN:`*[memOria](https://memoria-oclock.netlify.app/)* is a **flashcard application** designed for developers.
 \\
 There, I was responsible for the *front-end* and worked with [@S-Coulibally](https://github.com/S-COULIBALY) and [@VincentMingam](https://github.com/vmingam) on the *back-end*. 
 \\
 **Techno** :  CSS  - SASS  / JS - React / node.js – express / SQL – postgres
 [![Github](./logo/github.png)](https://github.com/memOria-project/memOria)`,
-DE:`*memOria* ist eine für Entwickler gedachte **Anwendung von Lernkarteien**: 
+DE:`*[memOria](https://memoria-oclock.netlify.app/)* ist eine für Entwickler gedachte **Anwendung von Lernkarteien**: 
 \\
 Dort bin ich für das *Front-End* zuständig und arbeite mit [@S-Coulibally](https://github.com/S-COULIBALY) und [@VincentMingam](https://github.com/vmingam) an dem *Back-End* zusammen. 
 \\
@@ -29,7 +29,7 @@ Dort bin ich für das *Front-End* zuständig und arbeite mit [@S-Coulibally](htt
 
 export const whatsthat = (<SetTranslate>
   {{
-  FR:`*What’s that* est une plateforme communautaire afin d’aider les développeurs à mieux décrire leur code, et recevoir de l’aide !
+  FR:`*[What’s that](https://whatsthat.netlify.app/)* est une plateforme communautaire afin d’aider les développeurs à mieux décrire leur code, et recevoir de l’aide !
   \\
   Le projet est un prototype, que j'utilise comme base de données personnelles mais j'espère pouvoir le partager avec le plus grand nombre à l'avenir.
   \\
@@ -38,7 +38,7 @@ export const whatsthat = (<SetTranslate>
   **Techno**: CSS  - Material-UI  / JS - React / node.js – express / MongoDB -mongoose
   [![Github](./logo/github.png)](https://github.com/JMJHautier/Whatsthat)
   `,
-  EN:`*What's that* is a community platform to help developers describe and name their code more precisely and thus get targeted help. 
+  EN:`*[What’s that](https://whatsthat.netlify.app/)* is a community platform to help developers describe and name their code more precisely and thus get targeted help. 
   \\
   The project is a prototype that I use as a personal database but hope to share with as many people as possible in the future. 
   \\
@@ -47,7 +47,7 @@ export const whatsthat = (<SetTranslate>
   **Techno**: CSS  - Material-UI  / JS - React / node.js – express / MongoDB -mongoose
   [![Github](./logo/github.png)](https://github.com/JMJHautier/Whatsthat)
   `,
-  DE:`*What's that* ist eine Gemeinschaftsplattform, um Entwicklern zu helfen, ihren Code genauer zu beschreiben, zu benennen und so zielgerichtet Hilfe zu erhalten.
+  DE:`*[What’s that](https://whatsthat.netlify.app/)* ist eine Gemeinschaftsplattform, um Entwicklern zu helfen, ihren Code genauer zu beschreiben, zu benennen und so zielgerichtet Hilfe zu erhalten.
   Das Projekt ist ein Prototyp, den ich als persönliche Datenbank verwende, aber hoffe, es in Zukunft mit möglichst vielen Menschen teilen zu können.
   Dort habe ich unter der Aufsicht von [@jorgePaul](https://github.com/weyvern) *Front* und *Back* selbständig programmiert: *Back* ist mit *node.js* und *MongoDB* und *Front* mit *React* und *Material-UI* erstellt worden.
   **Techno**: CSS  - Material-UI  / JS - React / node.js – express / MongoDB -mongoose
